77f4d1d9a566e12c6a9b2e566aae1d70a10ee314
Will Springer <skirmisher@protonmail.com>
2019-04-26 14:22:56 -0700

    Initial PoC sending battery charge ratio over TCP/Unix socket as ASCII
    Destination host/port can be configured via BATT_POST_ADDR env value. Can
    take the form of "IP:port" or "unix:/path/to/socket". Defaults to
    "127.0.0.1:34254".

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 use std::thread;
 use std::time::Duration;
+use std::io::prelude::*;
+use multisock::{SocketAddr,Stream};
+use std::env;
 
 fn main() -> battery::Result<()> {
     let manager = battery::Manager::new()?;
@@ -16,8 +19,18 @@ fn main() -> battery::Result<()> {
         }
     };
 
+    // Config
+    let addr = env::var("BATT_POST_ADDR")
+        .unwrap_or(String::from("127.0.0.1:34254"));
+
+    // Set up socket and buffer
+    let mut stream = Stream::connect(&addr.parse().unwrap())?;
+    let mut buf = String::new();
+
     loop {
-        println!("{:?}", battery);
+        buf = battery.state_of_charge().value.to_string();
+        stream.write_all(&buf.into_bytes())?;
+        stream.write_all(b"\n")?;
         thread::sleep(Duration::from_secs(1));
         manager.refresh(&mut battery)?;
     }
